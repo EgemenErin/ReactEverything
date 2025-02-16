5f067509a77fd6ac7d92ad5b774814aa29993e95
Egemen Erin <113554575+EgemenErin@users.noreply.github.com>
2025-02-16 20:21:56 +0100

    Add files via upload

---
+++ Day2/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>List of my favorite foods</h1>
+    <ul>
+      <li>Mercimek Corbasi</li>
+      <li>Tavuk Sote Pilav</li>
+      <li>Manti</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
