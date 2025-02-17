cfc7fed04ef3e5f760311c13af3da3221ee63094
Egemen Erin <113554575+EgemenErin@users.noreply.github.com>
2025-02-17 23:33:00 +0100

    Add files via upload

---
+++ Day7/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Heading from "./heading";
+import List from "./list";
+ReactDOM.render(
+  <div>
+    <Heading />
+    <List />
+  </div>,
+  document.getElementById("root")
+);
