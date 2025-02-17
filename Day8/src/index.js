926753256a236e644e423506c8a2230abcd7729e
Egemen Erin <113554575+EgemenErin@users.noreply.github.com>
2025-02-17 23:40:28 +0100

    Add files via upload

---
+++ Day8/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./../public/styles.css";
+import Heading from "./heading";
+import App from "./app";
+
+ReactDOM.render(<App />, document.getElementById("root"));
